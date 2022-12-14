4f888603bbc9adde6ec919b56ab5e332c07f3ee6
ItorK <karol.przybylski@esm-technology.pl>
2022-12-15 00:05:39 +0100

    IPQ8074: delete old patches

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -120,6 +120,7 @@ define Device/zyxel_nbg7815
 	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
 	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
 	DEVICE_PACKAGES := ipq-wifi-zyxel_nbg7815 kmod-ath11k-pci e2fsprogs kmod-fs-ext4 losetup kmod-hwmon-tmp103 \
-	kmod-bluetooth
+	kmod-bluetooth kmod-usb-storage
+	
 endef
 TARGET_DEVICES += zyxel_nbg7815
